44afd08f0f9e166ac7bb6a59dc420c9257f18622
ketansarode17 <91718807+ketansarode17@users.noreply.github.com>
2023-06-09 14:54:10 +0530

    Added solution - LeetHub

---
+++ Postfix to Infix Conversion - GFG/postfix-to-infix-conversion.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string postToInfix(string pre_exp) {
+        // Write your code here
+        int n=pre_exp.size();
+        stack<string>st;
+        for(int i=0;i<n;i++)
+        {
+            if(pre_exp[i]=='*' or pre_exp[i]=='+' or pre_exp[i]=='/' or pre_exp[i]=='-' or pre_exp[i]=='^')
+            {
+                string s1=st.top();st.pop();
+                string s2=st.top();st.pop();
+                st.push("("+s2+pre_exp[i]+s1+")");
+            }
+            else
+            {
+                string temp="";
+                temp+=pre_exp[i];
+                st.push(temp);
+            }
+        }
+        return st.top();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        string postfix;
+        cin >> postfix;
+        // char marker; cin >> marker;
+
+        Solution obj;
+        cout << obj.postToInfix(postfix) << endl;
+
+        // cout << "~\n";
+    }
+    fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends

d0c978222b56e02c1ec55ed3e4962aa1d6788eb6
ketansarode17 <91718807+ketansarode17@users.noreply.github.com>
2023-06-26 09:04:18 +0530

    Added solution - LeetHub

---
+++ nCr - GFG/n-cr.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    private:
+    int mod=1e9+7;
+    int fact(int num)
+    {
+        if(num==1 or !num)return 1;
+        return (num%mod*1LL*fact(num-1)%mod)%mod;
+    }
+    int modInverse(int A, int M)
+{
+    int m0 = M;
+    int y = 0, x = 1;
+ 
+    if (M == 1)
+        return 0;
+ 
+    while (A > 1) {
+        // q is quotient
+        int q = A / M;
+        int t = M;
+ 
+        // m is remainder now, process same as
+        // Euclid's algo
+        M = A % M, A = t;
+        t = y;
+ 
+        // Update y and x
+        y = x - q * y;
+        x = t;
+    }
+ 
+    // Make x positive
+    if (x < 0)
+        x += m0;
+ 
+    return x;
+}
+public:
+    int nCr(int n, int r){
+        // code here
+        if(r>n)return 0;
+        int numerator=fact(n);
+        int denominator=(fact(n-r)%mod*1LL*fact(r)%mod)%mod;
+        return (modInverse(denominator,mod)%mod*1LL*numerator%mod)%mod;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, r;
+        cin>>n>>r;
+        
+        Solution ob;
+        cout<<ob.nCr(n, r)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
